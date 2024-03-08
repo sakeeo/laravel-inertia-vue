eac8c910dea7bd36e236d1b7256d594d27400c16
muhammad Lim <ig.muhammadlim@gmail.com>
2024-03-08 15:36:09 +0800

    finish vfor data from database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        Post::factory(20)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container mx-auto pt-5">
+        <div class="post_block" v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.author }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+// import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    posts: Object,
+});
+</script>
